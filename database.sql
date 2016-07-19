84d5f0b82561d7fb8fe930c23caa9ceda5fefa75
Hillary Manning <hillary.b.manning@gmail.com>
2016-07-19 17:42:53 -0500

    style and add database.sql file

---
+++ database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "public"."words" (
+    "id" serial,
+    "adjective" text,
+    "noun" text,
+    PRIMARY KEY ("id")
+);
+
+INSERT INTO words (adjective, noun) VALUES
+('Sloppy','Clown'),
+('Rebellious','Zebra'),
+('Mythic','Milk'),
+('Expansive','Cherri'),
+('Explosive','Snack'),
+('Sad','Treat'),
+('Awesome','Meal'),
+('Spicy','Cloak'),
+('Clumsy','Frog'),
+('Weird','Monkey'),
+('Oafish','Valley'),
+('Shocking','Hill'),
+('Pointy','Mountain'),
+('Chill','Sky'),
+('Flaming','Pit'),
+('Sharp','Bull'),
+('Stealth','Barge'),
+('Oblong','Trash'),
+('Green','Carafe'),
+('Sneaky','Widget'),
+('Crumpled','Bird');
